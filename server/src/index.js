e8648d51e1b7ae91bf1d58805643d5b18e3f8e2c
Yash Godara <yash17godara@gmail.com>
2023-04-06 03:05:59 +0530

    Router setup
    update

--- server/src/index.js
+++ server/src/index.js
@@ -2,10 +2,15 @@ import express from 'express';
 import cors from 'cors';
 import mongoose from 'mongoose';
 
+import { userRouter } from './routes/users.js';
+
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter)
 
 mongoose.connect(
   "mongodb+srv://yash:yCzu0LYPllayrQSU@recipes.iav4fig.mongodb.net/recipes?retryWrites=true&w=majority"
---
+++ server/src/routes/users.js
@@ -0,0 +1,16 @@
+import express from "express";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+
+const router = express.Router();
+
+
+export { router as userRouter };
+
+
+
+
+
+
+
+
