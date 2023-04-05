7bb75021c9936dd68fc55517d21dcede606160a6
Yash Godara <yash17godara@gmail.com>
2023-04-06 01:55:00 +0530

    mongoose connect
    mongoose connect url setup : working

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+mongoose.connect(
+  "mongodb+srv://yash:yCzu0LYPllayrQSU@recipes.iav4fig.mongodb.net/recipes?retryWrites=true&w=majority"
+  );
+
 app.listen(3001, () =>
   console.log('SERVER STARTED AT PORT: 3001'),
 );
